80a994315aa1699f77ac88e667e6d77d4cfffa0f
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-10-18 19:26:15 +0200

    add core and scripts, updated the file paths

--- qtile/config.py
+++ qtile/config.py
@@ -13,26 +13,14 @@ from libqtile.utils import guess_terminal
 
 import subprocess
 
-##################################
-#########autostartSmth############
-##################################
-
-#@hook.subscribe.startup_once
-
-#def autostart():
-
-#home = os.path.expanduser('~/.config/qtile/autostart.sh')
-
-#subprocess.run(["picom"])
-
 @hook.subscribe.startup_once
 def autostart():
-    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    home = os.path.expanduser('~/.config/qtile/scripts/autostart.sh')
     subprocess.Popen([home])
 
-##################################
-#########settings#################
-##################################
+#---------------#
+#   SETTINGS    #
+#---------------#
 
 mod = "mod4"
 terminal = "kitty"
@@ -44,9 +32,9 @@ rofi = "rofi -show drun"
 #terminal = guess_terminal()
 
 
-################################
-############keys################
-################################
+#---------------#
+#  KEYBINDINGS  #
+#---------------#
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
@@ -142,7 +130,7 @@ for i in groups:
     )
 
 #---------------------------#
-#   WINDOW STYLE IN LAYOUTS #
+#        LAYOUTS            #
 #---------------------------#
 
 layouts = [
@@ -174,7 +162,7 @@ extension_defaults = widget_defaults.copy()
 screens = [
     Screen(
         
-        wallpaper = "~/.config/qtile/wallpaper.jpg",
+        wallpaper = "~/.config/qtile/Images/wallpaper.jpg",
         wallpaper_mode = "stretch",
 
         top=bar.Bar(
@@ -211,7 +199,7 @@ screens = [
                 ),
                 widget.Image(
                     background="#EDA3B1",
-                    filename="~/.config/qtile/lucyProfilePicTransparent.png",
+                    filename="~/.config/qtile/Images/lucyProfilePicTransparent.png",
                     scale="true"
                 ),
                 widget.GroupBox(
@@ -329,92 +317,6 @@ screens = [
     ),
 ]
 
-##################################
-########widgets/screens###########
-##################################
-#widget_defaults = dict(
-#    font="JetBrainsMono",
-#    foreground="#EDA3B1",
-#    fontsize=12,
-#    padding=2,
-#)
-#extension_defaults = widget_defaults.copy()
-#
-#screens = [
-#    Screen(
-#        wallpaper="~/.config/qtile/wallpaper.jpg",
-#        wallpaper_mode="stretch",
-#
-#        top=bar.Bar(
-#            [
-#                #left
-#                widget.Spacer(20),
-#                widget.TextBox("♥ |"),
-#                widget.GroupBox(),
-#                widget.TextBox("| ᓚᘏᗢ"),
-#                widget.Systray(),
-#                widget.TextBox("|"),
-#                widget.Prompt(),
-#                widget.TextBox("ᘛ⁐̤ᕐᐷ |"),
-#                
-#                widget.Spacer(),
-#                
-#                #right
-#                widget.Backlight(),
-#                widget.TextBox("| ⌛"),
-#                widget.Battery(),
-#                widget.TextBox("| ⌚"), #Unicode = U+1F551
-#                widget.Clock(),
-#                widget.TextBox("| ♬"), #Unicode = U#1F50A
-#                widget.Volume(),
-#                widget.TextBox("| ♥"),
-#                widget.Spacer(20),
-#            ],
-#            24,
-#            background="#323232",
-#            border_color="#EDA3B1",
-#            border_width= 2 ,  # Draw top and bottom borders
-#            margin = [5,5,5,5],
-#            opacity = 0.75
-#        ),
-#    ),
-#]
-#
-# Drag floating layouts.
-#mouse = [
-#    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-#    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-#    Click([mod], "Button2", lazy.window.bring_to_front()),
-#]
-#
-#dgroups_key_binder = None
-#dgroups_app_rules = []  # type: list
-#follow_mouse_focus = True
-#bring_front_click = False
-#cursor_warp = False
-#floating_layout = layout.Floating(
-#    float_rules=[
-#        # Run the utility of `xprop` to see the wm class and name of an X client.
-#        *layout.Floating.default_float_rules,
-#        Match(wm_class="confirmreset"),  # gitk
-#        Match(wm_class="makebranch"),  # gitk
-#        Match(wm_class="maketag"),  # gitk
-#        Match(wm_class="ssh-askpass"),  # ssh-askpass
-#        Match(title="branchdialog"),  # gitk
-#        Match(title="pinentry"),  # GPG key password entry
-#    ]
-#)
-#auto_fullscreen = True
-#focus_on_window_activation = "smart"
-#reconfigure_screens = True
-#
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
-#auto_minimize = True
-#
-# When using the Wayland backend, this can be used to configure input devices.
-#wl_input_rules = None
-
 #-----------------------#
 #   FLOATING WINDOWS    #
 #-----------------------#
@@ -449,16 +351,6 @@ auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
