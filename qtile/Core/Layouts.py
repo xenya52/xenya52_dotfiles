4028d4a57183ca1593c009d17a46286664c48dfa
xenya52 <108221381+xenya52@users.noreply.github.com>
2023-10-20 19:36:54 +0200

    deleted Bar.py, Windows.py, Workspaces.py; Set up Keybindings and Layouts and bind it into the main config

--- qtile/Core/Layouts.py
+++ qtile/Core/Layouts.py
@@ -18,3 +18,18 @@ layouts = [
     layout.Max(**layout_conf),
     layout.Columns(**layout_conf),
 ]
+
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ],
+    border_focus="#9ccfd8",
+    border_normal="#31748f"
+)
