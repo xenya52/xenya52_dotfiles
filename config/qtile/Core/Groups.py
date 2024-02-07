e9535c4e21e089dc52e49940c54a0c7645a7b63e
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-02-07 21:30:52 +0100

    add fourth window

--- config/qtile/Core/Groups.py
+++ config/qtile/Core/Groups.py
@@ -9,9 +9,9 @@ from libqtile.command import lazy
 
 groups = []
 
-group_names = ["1", "2", "3"]
-group_labels = ["[ぜ]", "[に]", "[ぁ]"]
-group_layouts = ["tile", "tile", "tile"]
+group_names = ["1", "2", "3", "4"]
+group_labels = ["[ぜ]", "[に]", "[ぁ]", "[:D]"]
+group_layouts = ["tile", "tile", "tile", "tile"]
 
 for i in range(len(group_names)):
     groups.append (
