85c2117af6a6e447eb9e4474b515d56fe5daf093
Lucy Hühn <666nikkineko@gmail.com>
2025-05-06 22:59:51 +0200

    feature / changes in neovim_2025 - adjust colorscheme, folders are pink now - add gitignore, for upcomming uncessesary files explicit in my neovim config - add flutter plugin, to code flutter in neovim, because i wanna try a bunch of things - 🥚 tomorrow is my thesis for my current education 🍀

--- 03_2025_nvim/lua/plugins/colorscheme.lua
+++ 03_2025_nvim/lua/plugins/colorscheme.lua
@@ -8,5 +8,11 @@ return {
     opts = {
       colorscheme = "sunbather",
     },
+    config = function(_, opts)
+      require("lazyvim").setup(opts)
+
+      -- Überschreibe die Farben für Ordner und verschiedene Icons
+      vim.cmd("highlight Directory guifg=#EDA3B1")
+    end,
   },
 }
---
+++ 03_2025_nvim/lua/plugins/flutter.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional, für bessere UI
+    },
+    config = true,
+    ft = "dart",
+  },
+}
